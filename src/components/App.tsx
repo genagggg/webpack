64e48098fe097aa2c2930eb01e0e42a5bffd4fc9
genagggg <mr.genag@mail.ru>
2025-04-25 17:42:59 +0500

    Рефактор роутов

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,22 @@
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Posts } from "../pages/Posts";
 import About from "../pages/About";
-
+import styles from "./App.module.scss";
 export default function App() {
   return (
     <HashRouter>
+      <div className={styles.navbar}>
+        <div className={styles.navbar__links}>
+          <a href="#/about">About</a>
+          <a href="#/posts">Posts</a>
+        </div>
+      </div>
+      <div className={styles.App}>
       <Routes>
-        <Route path="/" element={<Posts />} />
+        <Route path="/posts" element={<Posts />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      </div>
     </HashRouter>
   );
 }
